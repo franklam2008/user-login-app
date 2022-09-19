e41ed6e66cf843aab239b4c3b8e8fc55a1074e80
Frank Lam <franklam2008@gmail.com>
2022-09-18 23:48:25 -0500

    fix store reducer

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -12,13 +12,13 @@ export default {
     SET_USER(state: any, payload: string) {
       state.detail = payload;
     },
-    SET_STUDENT_LIST(state: any, payload: string) {
-      state.student_list = payload;
+    SET_STUDENT_LIST(state: any, payload: object) {
+      state.student_list = Object.values(payload);
     },
   },
   actions: {
-    saveName({ commit }: { commit: any }, data: any) {
-      commit("SET_NAME", data);
+    saveName({ commit }: { commit: any }, name: string) {
+      commit("SET_NAME", name);
     },
     loadedUser({ commit }: { commit: any }, data: any) {
       commit("SET_USER", data);
